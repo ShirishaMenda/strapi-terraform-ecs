0caa8bf57e27f13631a8b0f857a129a11176bdaf
sirigithubmenda <shirishamenda31998@gmail.com>
2026-02-18 00:41:52 +0530

    Fix pipeline order

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,5 +1,10 @@
+# Get latest ECS-optimized Amazon Linux 2 AMI
+data "aws_ssm_parameter" "ecs_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
 resource "aws_instance" "ecs_instance" {
-  ami           = "ami-0b6c6ebed2801a5cb"
+  ami           = data.aws_ssm_parameter.ecs_ami.value
   instance_type = "t3.micro"
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
