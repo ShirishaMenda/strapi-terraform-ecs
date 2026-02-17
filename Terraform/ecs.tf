2671d73a7b4e4fdf674f31763d0e796be7a042f3
sirigithubmenda <shirishamenda31998@gmail.com>
2026-02-17 23:11:27 +0530

    Fix pipeline order

--- Terraform/ecs.tf
+++ Terraform/ecs.tf
@@ -53,4 +53,18 @@ resource "aws_ecs_service" "strapi_service" {
   cluster         = aws_ecs_cluster.strapi_cluster.id
   task_definition = aws_ecs_task_definition.strapi_task.arn
   desired_count   = 1
+  launch_type     = "EC2"
+
+  force_new_deployment = true
+
+  depends_on = [
+    aws_ecs_cluster.strapi_cluster,
+    aws_ecs_task_definition.strapi_task
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      task_definition
+    ]
+  }
 }
