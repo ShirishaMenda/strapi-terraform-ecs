6c5d926df69425c777c6210be2b502c1c09ed5d3
sirigithubmenda <shirishamenda31998@gmail.com>
2026-02-18 15:40:14 +0530

    added files

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,25 +1,60 @@
+########################################
+# VPC
+########################################
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "strapi-vpc"
+  }
 }
+
+########################################
+# PUBLIC SUBNET A
+########################################
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "strapi-public-a"
+  }
 }
+
+########################################
+# PUBLIC SUBNET B
+########################################
 
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "strapi-public-b"
+  }
 }
 
+########################################
+# INTERNET GATEWAY
+########################################
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "strapi-igw"
+  }
 }
+
+########################################
+# PUBLIC ROUTE TABLE
+########################################
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
@@ -27,6 +62,10 @@ resource "aws_route_table" "public_rt" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "strapi-public-rt"
   }
 }
 
@@ -40,18 +79,89 @@ resource "aws_route_table_association" "public_b_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+########################################
+# PRIVATE SUBNET A
+########################################
+
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "strapi-private-a"
+  }
 }
+
+########################################
+# PRIVATE SUBNET B
+########################################
 
 resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "strapi-private-b"
+  }
 }
 
+########################################
+# NAT GATEWAY (for private subnets)
+########################################
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "strapi-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id   # NAT must be in PUBLIC subnet
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "strapi-nat-gateway"
+  }
+}
+
+########################################
+# PRIVATE ROUTE TABLE (via NAT)
+########################################
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "strapi-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_a_assoc" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_b_assoc" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+########################################
+# RDS SUBNET GROUP (Private Only)
+########################################
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name = "strapi-rds-subnet-group"
