2218734de9158baecd46ecb76432c9071fea1f9d
sirigithubmenda <shirishamenda31998@gmail.com>
2026-02-18 09:45:20 +0530

    Fix pipeline order

--- Terraform/ecs.tf
+++ Terraform/ecs.tf
@@ -1,3 +1,44 @@
+############################
+# ECS CLUSTER
+############################
+resource "aws_ecs_cluster" "strapi_cluster" {
+  name = "strapi-cluster"
+}
+
+############################
+# CLOUDWATCH LOG GROUP
+############################
+resource "aws_cloudwatch_log_group" "strapi_logs" {
+  name              = "/ecs/strapi"
+  retention_in_days = 7
+}
+
+############################
+# ECS TASK EXECUTION ROLE
+############################
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+############################
+# ECS TASK DEFINITION
+############################
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
   requires_compatibilities = ["EC2"]
@@ -5,7 +46,7 @@ resource "aws_ecs_task_definition" "strapi_task" {
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = "arn:aws:iam::138383657644:role/ec2-ecr-role"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -35,11 +76,34 @@ resource "aws_ecs_task_definition" "strapi_task" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = "/ecs/strapi"
+          awslogs-group         = aws_cloudwatch_log_group.strapi_logs.name
           awslogs-region        = "us-east-1"
           awslogs-stream-prefix = "ecs"
         }
       }
     }
   ])
+
+  depends_on = [
+    aws_cloudwatch_log_group.strapi_logs,
+    aws_iam_role_policy_attachment.ecs_task_execution_policy
+  ]
+}
+
+############################
+# ECS SERVICE
+############################
+resource "aws_ecs_service" "strapi_service" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent         = 100
+
+  depends_on = [
+    aws_ecs_task_definition.strapi_task
+  ]
 }
